28f457968ade549d7bfba90edabe7abdd8eeb8aa
kndwin <tnkevino@gmail.com>
2020-04-12 16:43:11 +1000

    added express to serve react on heroku

---
+++ server/build/precache-manifest.7b32ffd8a5dbc4d49c6768fcb05d9f35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3f9ffac7adcb685be0cfe5a9c42e41",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f114685e0175e272121b",
+    "url": "/static/css/main.ff6265ce.chunk.css"
+  },
+  {
+    "revision": "0c0aa3c1333692bae32f",
+    "url": "/static/js/2.227a4906.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.227a4906.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f114685e0175e272121b",
+    "url": "/static/js/main.f1c2842f.chunk.js"
+  },
+  {
+    "revision": "297839e78b7b91a03271",
+    "url": "/static/js/runtime-main.505e6735.js"
+  },
+  {
+    "revision": "0461b0a40188dd6b351a92d23127d5da",
+    "url": "/static/media/nadia.0461b0a4.jpg"
+  }
+]);
